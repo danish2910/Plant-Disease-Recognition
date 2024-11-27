9272c3ec9dd58d41e080a1e9e61449a3edc48013
Danish Azani <mdanish02@gmail.com>
2024-11-27 11:03:24 +0800

    title change

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Onboarding Screen',
+      title: 'Onboarding',
       home: OnboardingScreen(),
       debugShowCheckedModeBanner: false,
     );
